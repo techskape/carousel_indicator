54791fe1763ffa0af12def06af9358f62dd3b5d2
Love K <techskape@gmail.com>
2020-08-06 17:36:37 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:carouselindicator/carousel_indicator.dart';
+import 'package:carousel_indicator/carousel_indicator.dart';
 
 void main() {
  test('adds one to input values', () {});
